ed4430c297f9cf14d5b72695bf9760337a68a8d7
itsamanverma <amanvermame786@gmail.com>
2020-04-25 20:05:51 +0530

    change the setup location

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+    render() {
+      return (
+        <div>
+            Hello Burger Builder 
+        </div>
+      ); 
+    }
+}
+    export default App; 
