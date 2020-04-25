987cd1d28e8e641ac1c850c782d82cc68bf457fb
itsamanverma <amanvermame786@gmail.com>
2020-04-25 23:56:24 +0530

    creating the component stateful & stateless based on requirement

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
 
     render() {
       return (
         <div>
-            Hello Burger Builder 
+          <Layout>
+            <p>test</p>
+          </Layout>
         </div>
       ); 
     }
 }
-    export default App; 
+
+export default App; 
