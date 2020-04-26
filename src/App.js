ef7087bd51b04346bb6d0d33f1de68d3a7840ab4
itsamanverma <amanvermame786@gmail.com>
2020-04-26 23:46:23 +0530

    Burger Component & SubComponent as BurgerIngredient

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
 
@@ -8,7 +9,7 @@ class App extends Component {
       return (
         <div>
           <Layout>
-            <p>test</p>
+            <BurgerBuilder /> 
           </Layout>
         </div>
       ); 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Wrapper from '../../hoc/Wrapper';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Wrapper>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className = {classes.Content}>
             {props.children}
         </main>
     </Wrapper>
