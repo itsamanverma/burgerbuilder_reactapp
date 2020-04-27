38353da241ccc91af934507f67292f5fbcb90963
itsamanverma <amanvermame786@gmail.com>
2020-04-28 01:02:11 +0530

    proper use arrow function, map & reduce method

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
-    const transformedIngredient = Object.keys(props.ingredients)
+    let transformedIngredient = Object.keys(props.ingredients)
     .map( igKey => {
         return [...Array(props.ingredients[igKey])].map((_,i) => {
             return <BurgerIngredient key={igKey + i} type = {igKey} />
         });
-    });
-
+    })
+    .reduce((arr,el) => {
+        return arr.concat(el)
+    }, []);
+    if(transformedIngredient.length === 0) {
+        transformedIngredient = <p>Please start adding Ingredients!</p>
+    }
+    console.log(transformedIngredient);
     return (
         <div className = {classes.Burger}>
             <BurgerIngredient type="bread-top"/>
