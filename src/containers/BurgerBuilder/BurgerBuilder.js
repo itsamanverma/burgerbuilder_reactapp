a19664f13ded6a51392eb822cf660bad7c46a950
itsamanverma <amanvermame786@gmail.com>
2020-04-27 23:02:31 +0530

    ways of passing the BurgerIngredient in dynamic ways

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Wrapper from '../../hoc/Wrapper';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
+
+    state = {
+        ingredients:{
+            salad:1,
+            bacon:1,
+            cheese:2,
+            meat:2
+        }
+    }
     render () {
         return (
             <Wrapper>
-                <Burger />
+                <Burger ingredients = {this.state.ingredients }/>
                 <div>Build Controls</div>
             </Wrapper>
         );
