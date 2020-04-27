39017cc7d4dcfc5c10828d43fc75db19513074ee
itsamanverma <amanvermame786@gmail.com>
2020-04-27 19:20:58 +0530

    creating the Burger Component & BurgerIngredient component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Wrapper from '../../hoc/Wrapper';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
     render () {
         return (
             <Wrapper>
-                <div>Burger</div>
+                <Burger />
                 <div>Build Controls</div>
             </Wrapper>
         );
