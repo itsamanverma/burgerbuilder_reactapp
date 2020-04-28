ebbae6d61077a629ec7bbdfe92c3ca2c3a5399d1
itsamanverma <amanvermame786@gmail.com>
2020-04-29 01:53:54 +0530

    adding the disablng the button based the condition

--- src/components/BuildControls/BuildControl/BuildControl.js
+++ src/components/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,15 @@ const buildControl = (props) => (
 
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less} onClick={props.removed}>Less</button>
-        <button className={classes.More} onClick ={props.added}>More</button>
+        <button 
+            className={classes.Less}
+            onClick={props.removed} 
+            disabled={props.disabled}>Less
+        </button>
+        <button 
+            className={classes.More} 
+            onClick ={props.added}>More
+        </button>
     </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -41,6 +41,9 @@ class BurgerBuilder extends Component{
     
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0) {
+            return;
+        }
         const updatedCount = oldCount - 1;
         const updatedIngredients = {
             ...this.state.ingredients
@@ -56,12 +59,19 @@ class BurgerBuilder extends Component{
     }
 
     render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for( let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <=0
+        }
         return (
             <Wrapper>
                 <Burger ingredients = {this.state.ingredients }/>
                 <BuildControls 
                 ingredientAdded = {this.addIngredientHandler}
-                ingredientRemoved = {this.removeIngredientHandler} />
+                ingredientRemoved = {this.removeIngredientHandler}
+                disabled = {disabledInfo} />
             </Wrapper>
         );
     }
