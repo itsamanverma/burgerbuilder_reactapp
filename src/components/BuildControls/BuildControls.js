4ddf071e4e5d9815f1fba683935f3338fc56147c
itsamanverma <amanvermame786@gmail.com>
2020-04-29 15:27:00 +0530

    adding the order button in buildControls

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -20,6 +20,7 @@ const buildControls = (props) => (
                 removed ={() => props.ingredientRemoved(ctrl.type)}
                 disabled = {props.disabled[ctrl.type]} />
         ))}
+        <button className={classes.OrderButton}>ORDER NOW</button>
     </div>
 );
 
