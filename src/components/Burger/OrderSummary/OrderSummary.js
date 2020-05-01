d793b8ed3f90436f2fc2b6cd80f05697d038cb69
itsamanverma <amanvermame786@gmail.com>
2020-05-01 18:42:30 +0530

    create the navigation component & inside create the toolbar component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -13,11 +13,12 @@ const orderSummary = (props) => {
 
     return (
         <Wrapper>
-            <h3>Your Order!</h3>
+            <h3>Your Order!</h3>    
             <p>A delicious burger with following ingredients</p>
             <ul>
                 {ingredientsSummary}
             </ul>
+            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
             <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
             <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
