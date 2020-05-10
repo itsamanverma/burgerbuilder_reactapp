05a81afe9060f090e5e1b5407d4b700579988f56
itsamanverma <amanvermame786@gmail.com>
2020-05-10 23:20:15 +0530

    use the backdrop component & sidedrawer component

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,22 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../Toolbar/NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Bac..kdrop/Backdrop';
+import Wrapper from '../../../hoc/Wrapper';
 
 const sideDrawer = (props) => {
     return(
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Wrapper>
+            <Backdrop />
+            <div className={classes.SideDrawer}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Wrapper>
     );
 }
 
